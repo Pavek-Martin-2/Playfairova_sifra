d29f6fde3ce66d7029604632066f719604eabbcb
Martin Pávek <pavek_martin@seznam.cz>
2026-01-20 09:30:54 +0100

    Add files via upload

---
+++ prubeh_prekladu_jazyka_C/prubeh_prekladu_jazyka_C.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout<<endl<<"Prubeh prekladu v jazyce C"<<endl<<endl;
+  cout<<"  ----------"<<endl;
+  cout<<"  | Editor |"<<endl;
+  cout<<"  ----------"<<endl;
+  cout<<"      |"<<endl;
+  cout<<"      |"<<endl;
+  cout<<"   --------   -------"<<endl;
+  cout<<"   | .CPP |   | .H  |"<<endl;
+  cout<<"   --------   -------"<<endl;
+  cout<<"      |          |"<<endl;
+  cout<<"      |          |"<<endl;
+  cout<<"  -------------------"<<endl;
+  cout<<"  |  Preprocesor    |"<<endl;
+  cout<<"  -------------------"<<endl;
+  cout<<"  |    Prekladac    |"<<endl;
+  cout<<"  -------------------"<<endl;
+  cout<<"          |"<<endl;
+  cout<<"          |"<<endl;
+  cout<<"     ----------    ---------   --------"<<endl;
+  cout<<"     | .OBJ.O |   | .OBJ.O |   | .LIB |"<<endl;	
+  cout<<"     ----------    ---------   --------"<<endl;
+  cout<<"          |           |           |"<<endl;
+  cout<<"          |           |           |"<<endl;
+  cout<<"     ----------------------------------"<<endl;
+  cout<<"     |             LINKER             |"<<endl;
+  cout<<"     ----------------------------------"<<endl;	
+  cout<<"                      |"<<endl;
+  cout<<"                      |"<<endl;	
+  cout<<"                   ---------"<<endl;
+  cout<<"                   | .EXE  |"<<endl;
+  cout<<"                   ---------"<<endl<<endl;
+	
+	system("pause");
+	return 0;
+}
+
